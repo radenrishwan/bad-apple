c3142a4d2bf33db2aa34eef54eadcef9e83f8734
radenrishwan <radenrishwan@gmail.com>
2024-12-09 10:53:08 +0700

    add render frame

--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	badapple "github.com/radenrishwan/bad-apple"
+	"golang.org/x/term"
 )
 
 const (
@@ -18,6 +19,12 @@ const (
 )
 
 func main() {
+	// get terminal size
+	width, height, err := term.GetSize(int(os.Stdout.Fd()))
+	if err != nil {
+		log.Fatalln("error getting terminal size:", err)
+	}
+
 	// count the frames
 	fc, err := os.ReadDir(FRAME_PATH)
 	if err != nil {
@@ -35,15 +42,50 @@ func main() {
 			log.Fatalln(err)
 		}
 
-		// render frame
+		// scale the frame
+		frame = scaleFrame(frame, width, height)
+
 		renderFrame(frame)
 
 		time.Sleep(FRAME_DURATION)
 	}
 }
 
+func scaleFrame(frame [][]uint8, targetWidth, targetHeight int) [][]uint8 {
+	originalHeight := len(frame)
+	originalWidth := len(frame[0])
+
+	scaled := make([][]uint8, targetHeight)
+	for i := range scaled {
+		scaled[i] = make([]uint8, targetWidth)
+	}
+
+	// scale factors
+	scaleY := float64(originalHeight) / float64(targetHeight)
+	scaleX := float64(originalWidth) / float64(targetWidth)
+
+	// scale the frame
+	for y := 0; y < targetHeight; y++ {
+		sourceY := int(float64(y) * scaleY)
+		if sourceY >= originalHeight {
+			sourceY = originalHeight - 1
+		}
+
+		for x := 0; x < targetWidth; x++ {
+			sourceX := int(float64(x) * scaleX)
+			if sourceX >= originalWidth {
+				sourceX = originalWidth - 1
+			}
+
+			scaled[y][x] = frame[sourceY][sourceX]
+		}
+	}
+
+	return scaled
+}
+
 func renderFrame(frame [][]uint8) {
-	fmt.Print("\033[H\033[2J")
+	os.Stdout.Write([]byte("\033[H"))
 
 	for _, row := range frame {
 		for _, pixel := range row {
